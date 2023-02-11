8203ccfd8dc848afd6d9a20de2e76274d66cd41e
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2023-02-12 00:31:03 +0100

    requete bac done

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -150,49 +150,73 @@ app.post('/node/sub',(req,res)=>{
     
 });
 
+
 app.post('/rechercheProfil',(req,res)=>{
 
     console.log(req.body)
 
-    let allRequete;
+    let reponseRecherche;
+    let requeteSql;
 
     let statut = req.body.statut;
     let profil = req.body.profil;
     let region = req.body.region;
     let famille = req.body.famille;
-    let formationBac45 = req.body.formation;
+    let formation = req.body.formation;
     let domaine = req.body.domaine;
 
-    const connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'Crapulo2001*',
-        database: 'vivier'
-    });
 
-    let requeteSql = `SELECT * FROM mytable WHERE Statut LIKE "${statut}%"
-    AND Type_de_profil ="${profil}" 
-    AND Regions_de_residence_cibles ="${region}" 
-    AND Famille_de_profil_SAP = "${famille}"
-    AND Formation_initiale = "${formationBac45}" 
-    AND Domaine_SAP = "${domaine}"`
-
-    connection.query(
-
-        requeteSql,
-        function(err, results, fields) {
-        //   console.log(results);
-            allRequete = JSON.stringify(results);
-            // console.log(allRequete);
-            setTimeout(envoi,1);
-        }
-    )
+    if(formation == 'Bac+4/5'){
+        requeteSql = `SELECT * FROM mytable WHERE Statut LIKE "${statut}%"
+        AND Type_de_profil LIKE "${profil}%" 
+        AND Regions_de_residence_cibles LIKE "${region}%" 
+        AND Famille_de_profil_SAP LIKE "${famille}%"
+        AND (Formation_initiale LIKE "${formation}%" OR Formation_initiale LIKE "Bac+5%")
+        AND Domaine_SAP LIKE "${domaine}%"`
+        console.log("passe");
+        requeteBdd();
         
+    }else{
 
-    function envoi(){
-        // console.log(allRequete);
-        res.end(allRequete);
+        requeteSql = `SELECT * FROM mytable WHERE Statut LIKE "${statut}%"
+        AND Type_de_profil LIKE "${profil}%" 
+        AND Regions_de_residence_cibles LIKE "${region}%" 
+        AND Famille_de_profil_SAP LIKE "${famille}%"
+        AND Domaine_SAP LIKE "${domaine}%"`
+        requeteBdd();
     }
+
+
+    function requeteBdd(){
+
+        console.log("bdd");
+
+        const connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'Crapulo2001*',
+            database: 'vivier'
+        });
+    
+        connection.query(
+    
+            requeteSql,
+            function(err, results, fields) {
+            //   console.log(results);
+                reponseRecherche = JSON.stringify(results);
+                // console.log(reponseRecherche);
+                setTimeout(envoi,1);
+            }
+        )
+            
+    
+        function envoi(){
+            // console.log(reponseRecherche);
+            res.end(reponseRecherche);
+        }
+    }
+
+
 
 })
 
