687a2cec1736dce554078845ed6d3bfccd45cefa
BaptisteGrimaldi <grimaldi.baptiste@gmail.com>
2023-04-24 02:01:06 +0200

    token done

--- BackEnd/Front/log/log.js
+++ BackEnd/Front/log/log.js
@@ -24,52 +24,101 @@ testToken.addEventListener("click",()=>{
    })
    .then((res)=>{
       console.log(res);
+
+      try{
+
+         const d = new Date();
+         d.setTime(d.getTime() + (60 * 60 * 1000));
+         const expires = "expires=" + d.toUTCString();
+         console.log(expires);
+         document.cookie = `access_token=${res.access_token};${expires};path=/`;
+
+      }catch{
+         console.error("cookie pas set");
+      }
+      finally{
+
+         let formu ={}; 
+
+         formu ={"mdp":mdp.value,"gmail":mail.value};
+
+
+         fetch(`${env}/node/login`,{
+        
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json" 
+            },
+            body : JSON.stringify(formu)
+         })
+         .then((res)=>{
+           mail.value = "";
+           mdp.value = "";
+           return res.text();
+           
+         })
+           .then((res)=>{
+        
+              if(res == "false"){
+                 let p = document.getElementById("reponseServer");
+                 p.textContent = "Login incorrect";
+                 p.style.color = "red"; 
+                 reponseServer.style.visibility= "visible";
+              }else{
+                 window.location.assign(res);
+              }
+        
+           })
+
+      }
+
    })
    .catch((error)=>{
       console.log('Probleme avec le token',error);
    })
+   
 })
 
 
 
 
-let formu ={}; 
+// let formu ={}; 
 
-bouton.addEventListener("click",()=>{
-
-
- formu ={"mdp":mdp.value,"gmail":mail.value};
+// bouton.addEventListener("click",()=>{
 
 
- fetch(`${env}/node/login`,{
+//  formu ={"mdp":mdp.value,"gmail":mail.value};
 
-    method : "POST",
-    headers : {
-        "Content-Type" : "application/json" 
-    },
-    body : JSON.stringify(formu)
- })
- .then((res)=>{
-   mail.value = "";
-   mdp.value = "";
-   return res.text();
+
+//  fetch(`${env}/node/login`,{
+
+//     method : "POST",
+//     headers : {
+//         "Content-Type" : "application/json" 
+//     },
+//     body : JSON.stringify(formu)
+//  })
+//  .then((res)=>{
+//    mail.value = "";
+//    mdp.value = "";
+//    return res.text();
    
- })
-   .then((res)=>{
+//  })
+//    .then((res)=>{
 
-      if(res == "false"){
-         let p = document.getElementById("reponseServer");
-         p.textContent = "Login incorrect";
-         p.style.color = "red"; 
-         reponseServer.style.visibility= "visible";
-      }else{
-         window.location.assign(res);
-      }
+//       if(res == "false"){
+//          let p = document.getElementById("reponseServer");
+//          p.textContent = "Login incorrect";
+//          p.style.color = "red"; 
+//          reponseServer.style.visibility= "visible";
+//       }else{
+//          window.location.assign(res);
+//       }
 
-   })
+//    })
 
 
 
-}) //Fin bouton
+// }) //Fin bouton
 
 
--- BackEnd/server.js
+++ BackEnd/server.js
@@ -86,7 +86,7 @@ app.post('/createToken',(req,res)=>{
                 if(result === null){
                     res.status(401).json({ message: 'Invalid credentials' });
                 }else{
-                    const token = jwt.sign({ mail : req.body.gmail, mdp: req.body.mdp }, secret, { expiresIn: '1h' });
+                    const token = jwt.sign({}, secret, { expiresIn: '1h' });
                     res.json({ access_token: token });
                 }
 
